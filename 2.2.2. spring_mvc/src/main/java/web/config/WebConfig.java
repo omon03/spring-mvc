37ab7b89763306f8f6f7ce405dd45ba9f898af64
omon03 <omon03@gmail.com>
2021-04-11 12:46:08 +0500

    commit pom

--- 2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
+++ 2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
@@ -22,7 +22,6 @@ public class WebConfig implements WebMvcConfigurer {
         this.applicationContext = applicationContext;
     }
 
-
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
@@ -39,8 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
     }
-
-
+    
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
